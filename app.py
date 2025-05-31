d5b2a42377b67f1935215c099cfa826cc81ff52f
Ruth Daniels <ruthdaniel1705@gmail.com>
2025-06-01 02:17:37 +0300

    Day 4:Added Template Inheritance using Jinja

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask,render_template
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def home_page():
+    return render_template("home.html")
+
+@app.route('/market')
+def market_page():
+    items=[
+        {"id":1,"name":"phone","barcode":"893212299897","price":500},
+        {"id":2,"name":"laptop","barcode":"123512699897","price":900},
+        {"id":3,"name":"keyboard","barcode":"103275299997","price":150}
+    ]
+    return render_template("market.html",items=items)
+"""
+#Dynamic routing
+@app.route('/about/<username>')
+def about_page(username):
+    return f'<h1>This is the about page of {username}</h1>'
+"""
+if __name__ == '__main__':
+    app.run(debug=True)
